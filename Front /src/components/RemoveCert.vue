b99ab07224582c07f6d2ed797bc4e13e97f32a0b
Sachin Kumar <69132410+sachinsworld@users.noreply.github.com>
2023-01-14 13:04:57 +0530

    Add files via upload

---
+++ Front /src/components/RemoveCert.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="page">
+    <LoginNavBar />
+    <div class="cert">
+      <div id="ftr"><NavFeature /></div>
+      <div class="form">
+        <div><input type="text" placeholder="Roll No" v-model="rollNo" /></div>
+        <div><button v-on:click="delCert()">Delete</button></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import LoginNavBar from "./LoginNavBar.vue";
+import NavFeature from "./NavFeature.vue";
+import axios from "axios";
+export default {
+  name: "RemoveCert",
+  components: {
+    LoginNavBar,
+    NavFeature,
+  },
+  data() {
+    return {
+      rollNo: "",
+    };
+  },
+  methods: {
+    async delCert() {
+      let result = await axios.delete(
+        `https://certify-verify.onrender.com/certs/${this.rollNo}`
+      );
+      if (result.status == 200) {
+        alert("Certificate Deleted");
+      }
+      localStorage.setItem("user-info", JSON.stringify(result.data));
+      this.$router.push({ name: "AddCert" });
+    },
+  },
+};
+</script>
+<style scoped>
+.cert {
+  display: flex;
+  height: 90vh;
+}
+#ftr {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 0.8;
+}
+.form{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-grow: 1;
+}
+button{
+  padding: 10px;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+@media only screen and (max-width: 668px) {
+  .cert{
+    flex-direction: column;
+  }
+}
+</style>
---
+++ Front /src/components/viewAllCert.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <LoginNavBar />
+    <div>
+      <h1>All Certificates Issued</h1>
+      <table>
+        <thead>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Course</th>
+          <th>Email</th>
+          <th>Roll Number</th>
+          <th>date</th>
+        </thead>
+        <tbody>
+          <tr v-for="item in list" :key="item">
+            <td>{{ item.cert._id }}</td>
+            <td>{{ item.cert.name }}</td>
+            <td>{{ item.cert.course }}</td>
+            <td>{{ item.cert.email }}</td>
+            <td>{{ item.cert.rollNo }}</td>
+            <td>{{ item.cert.date }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginNavBar from "./LoginNavBar.vue";
+import axios from "axios";
+export default {
+  name: "viewAllCert",
+  components: {
+    LoginNavBar,
+  },
+  data() {
+    return {
+      list: [],
+    };
+  },
+  async mounted() {
+    let result = await axios.get(
+    "https://certify-verify.onrender.com/certs/"
+      //  "https://evening-inlet-19531.herokuapp.com/certs/"
+    );
+    this.list = result.data.users;
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: rgb(255, 255, 255);
+}
+table {
+  width: 100vw;
+}
+th,
+td,
+h1 {
+  text-align: center;
+}
+thead,
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+tr:nth-child(odd) {
+  background-color: #ffffff;
+}
+@media only screen and (max-width: 800px) {
+  th,td,h1 {
+  padding: 6px;
+}
+table{
+  font-size: 0.45em;
+}
+#n_deadspace{
+  width:70vw;
+}
+h1{
+  font-size: 0.7em;
+}
+}
+@media only screen and (min-width: 801px) {
+  th,td,h1 {
+  padding: 12px;
+}
+table{
+  font-size: 0.8em;
+}
+#n_deadspace{
+  width:70vw;
+}
+}
+</style>
